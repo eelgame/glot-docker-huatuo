2aae5ae506392a37ca0724aa5362bc7feaa0df9b
eelgame <eelgame@qq.com>
2022-06-14 17:27:42 +0800

    glot/lua@sha256:4395999c16dfc17c0ec9cf61ad3926c95890c25e51c7e2731ecaf9087a617a22

--- huatuo-glot/Dockerfile.lua
+++ huatuo-glot/Dockerfile.lua
@@ -1,5 +1,4 @@
-# docker build -f Dockerfile.glot -t glot/huatuo .
-FROM glot/lua
+FROM glot/lua@sha256:4395999c16dfc17c0ec9cf61ad3926c95890c25e51c7e2731ecaf9087a617a22
 
 ADD code-runner_linux-x64.tar.gz /
 
